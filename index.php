7cb26d1c9aa77281aa60aaa1229d01ac7e51a7d7
DaniSuarez115 <Daniel.Suarez@ucr.ac.cr>
2023-07-11 18:21:34 -0600

    comit interfaz
    7/11/2023

--- index.php
+++ index.php
@@ -10,13 +10,16 @@
   <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
+ <link rel="stylesheet" href="Style.css">
 </head>
 
 <body>
-  <header>
-   <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #e3f2fd;">
-       <a class="navbar-brand" href="#">Navbar</a>
+ <div class="fluid-container" id="backround">
+ <div>
+ <header>
+    <div class="fluid-container">
+    <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #000;">
+    <a class="navbar-brand" href="#">KaPaSem-API</a>
        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
            aria-expanded="false" aria-label="Toggle navigation"></button>
        <div class="collapse navbar-collapse" id="collapsibleNavId">
@@ -24,14 +27,11 @@
                <li class="nav-item">
                    <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
                </li>
-               <li class="nav-item">
-                   <a class="nav-link" href="#">Link</a>
-               </li>
                <li class="nav-item dropdown">
-                   <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                   <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Acciones</a>
                    <div class="dropdown-menu" aria-labelledby="dropdownId">
-                       <a class="dropdown-item" href="#">Action 1</a>
-                       <a class="dropdown-item" href="#">Action 2</a>
+                       <a class="dropdown-item" href="#">Usuario</a>
+                       <a class="dropdown-item" href="#">Plantas</a>
                    </div>
                </li>
            </ul>
@@ -40,11 +40,26 @@
                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
            </form>
        </div>
+    </div>
    </nav>
   </header>
-  <main>
+ </div>
+ <main>
+    <div class="titulo">
+        <?php
+          $rutaImagen='Imagenes/ivy.png'
+        ?>
+        <div class="ivy">
+              <img class="pokemon" src="<?php echo $rutaImagen; ?>" class="img-fluid rounded-top" alt="">
+        </div>
+      
+        <h1>Bienvenido a la API de KaPaSem</h1>
+    </div>
+    <div class="BodyMain">
+        <!-- Contenido del cuerpo principal -->
+    </div>
+</main>
 
-  </main>
   <footer>
     <!-- place footer here -->
   </footer>
@@ -56,6 +71,8 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
   </script>
+  
+ </div>
 </body>
 
 </html>
